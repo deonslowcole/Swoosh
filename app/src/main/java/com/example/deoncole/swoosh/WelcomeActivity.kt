3ab15ea5892e0cb269bed69a5efeab15cf6e5d1b
deonslowcole <dcole@fullsail.edu>
2018-07-04 18:51:47 -0400

    Create skill activity

--- app/src/main/java/com/example/deoncole/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/deoncole/swoosh/WelcomeActivity.kt
@@ -5,12 +5,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
+        //Don't need to set find by id; just use the id of the object and it will be imported
         getStartedBtn.setOnClickListener {
             val leagueIntent = Intent (this, LeagueActivity::class.java)
             startActivity(leagueIntent)
